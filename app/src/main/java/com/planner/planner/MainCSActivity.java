5e8c4907ed8a897e4c0a5904b4c72ffdab2a25a5
Daviddude <38021338+Daviddude@users.noreply.github.com>
2018-04-13 23:05:20 -0700

    Add files via upload

--- app/src/main/java/com/planner/planner/MainCSActivity.java
+++ app/src/main/java/com/planner/planner/MainCSActivity.java
@@ -3,17 +3,11 @@ package com.planner.planner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainCSActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_cs);
     }
-
-    //Testing only test test
-    //David L
-    //Ethan Luu
-    //hjgkjhkjn
-
 }
