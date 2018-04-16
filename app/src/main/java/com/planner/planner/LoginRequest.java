e9c2415bfeb0265c755e1563c548dffd4c408c73
David L <dongxli710@gmail.com>
2018-04-16 16:45:56 -0700

    sd

--- app/src/main/java/com/planner/planner/LoginRequest.java
+++ app/src/main/java/com/planner/planner/LoginRequest.java
@@ -3,7 +3,6 @@ package com.planner.planner;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
-
 import java.util.HashMap;
 import java.util.Map;
 
